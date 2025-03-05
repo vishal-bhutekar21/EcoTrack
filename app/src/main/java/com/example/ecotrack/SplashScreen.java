38c2386769045d3ebb5377820b9bb1a674ef0f4d
Chaitany <chaitnykakde517@gmail.com>
2025-03-05 11:42:58 +0530

    commit after packages

--- app/src/main/java/com/example/ecotrack/SplashScreen.java
+++ app/src/main/java/com/example/ecotrack/SplashScreen.java
@@ -1,12 +1,14 @@
 package com.example.ecotrack;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ecotrack.Authentication.Login;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,10 +17,19 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(insets.getInsets(WindowInsetsCompat.Type.systemBars()).left,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).top,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).right,
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
             return insets;
         });
+
+        // Delay for 1 second, then move to LoginActivity
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, Login.class));
+            finish(); // Close SplashScreen to prevent going back
+        }, 1000);
     }
 }
